d2e96a2217a2065bc316fec9848a9cdf79773a95
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-02-15 11:34:14 +0000

    Updated the postgresql database for the tests

--- tests/databases/postgresql.sql
+++ tests/databases/postgresql.sql
@@ -1,3 +1,72 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+CREATE SCHEMA crm;
+
+CREATE SCHEMA hr;
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+SET search_path = crm, pg_catalog;
+SET default_tablespace = '';
+SET default_with_oids = false;
+
+CREATE TABLE customers (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL,
+    employee_id integer NOT NULL
+);
+
+CREATE SEQUENCE customers_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE customers_id_seq OWNED BY customers.id;
+SET search_path = hr, pg_catalog;
+
+CREATE TABLE categories (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL
+);
+
+CREATE SEQUENCE departments_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE departments_id_seq OWNED BY categories.id;
+
+CREATE TABLE employees (
+    id integer NOT NULL,
+    firstname character varying(255) NOT NULL,
+    lastname character varying(255),
+    date_of_birth date
+);
+
+CREATE SEQUENCE employees_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE employees_id_seq OWNED BY employees.id;
+SET search_path = public, pg_catalog;
+
 CREATE TABLE departments (
     id integer NOT NULL,
     name character varying(255) NOT NULL
@@ -10,8 +79,8 @@ CREATE SEQUENCE departments_id_seq
     NO MAXVALUE
     CACHE 1;
 
-ALTER SEQUENCE departments_id_seq OWNED BY departments.id;
 
+ALTER SEQUENCE departments_id_seq OWNED BY departments.id;
 
 CREATE TABLE roles (
     id integer NOT NULL,
@@ -24,6 +93,7 @@ CREATE SEQUENCE roles_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
+
 
 ALTER SEQUENCE roles_id_seq OWNED BY roles.id;
 
@@ -50,13 +120,47 @@ CREATE SEQUENCE users_id_seq
     NO MAXVALUE
     CACHE 1;
 
+
 ALTER SEQUENCE users_id_seq OWNED BY users.id;
+
+CREATE VIEW users_view AS
+ SELECT users.id,
+    users.username,
+    users.password,
+    users.firstname,
+    users.lastname,
+    users.othernames,
+    users.email,
+    roles.name AS role
+   FROM (users
+     JOIN roles ON ((users.role_id = roles.id)));
+SET search_path = crm, pg_catalog;
+
+ALTER TABLE ONLY customers ALTER COLUMN id SET DEFAULT nextval('customers_id_seq'::regclass);
+SET search_path = hr, pg_catalog;
+
+ALTER TABLE ONLY categories ALTER COLUMN id SET DEFAULT nextval('departments_id_seq'::regclass);
+
+ALTER TABLE ONLY employees ALTER COLUMN id SET DEFAULT nextval('employees_id_seq'::regclass);
+SET search_path = public, pg_catalog;
 
 ALTER TABLE ONLY departments ALTER COLUMN id SET DEFAULT nextval('departments_id_seq'::regclass);
 
 ALTER TABLE ONLY roles ALTER COLUMN id SET DEFAULT nextval('roles_id_seq'::regclass);
 
 ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+SET search_path = crm, pg_catalog;
+
+ALTER TABLE ONLY customers
+    ADD CONSTRAINT customers_pkey PRIMARY KEY (id);
+SET search_path = hr, pg_catalog;
+
+ALTER TABLE ONLY categories
+    ADD CONSTRAINT departments_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY employees
+    ADD CONSTRAINT employees_pkey PRIMARY KEY (id);
+SET search_path = public, pg_catalog;
 
 ALTER TABLE ONLY departments
     ADD CONSTRAINT departments_pkey PRIMARY KEY (id);
@@ -66,10 +170,16 @@ ALTER TABLE ONLY roles
 
 ALTER TABLE ONLY users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+SET search_path = crm, pg_catalog;
+
+ALTER TABLE ONLY customers
+    ADD CONSTRAINT customers_employee_id_fkey FOREIGN KEY (employee_id) REFERENCES hr.employees(id);
+SET search_path = public, pg_catalog;
 
 ALTER TABLE ONLY users
     ADD CONSTRAINT users_office_fkey FOREIGN KEY (office) REFERENCES departments(id);
 
 ALTER TABLE ONLY users
     ADD CONSTRAINT users_role_id_fkey FOREIGN KEY (role_id) REFERENCES roles(id);
+
 
