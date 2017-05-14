6460ce87fc372f2da729d4bd38ec02e59cbb0e94
James Ainooson <jainooson@gmail.com>
2017-05-13 20:33:44 -0500

    Conforming to coding styles

--- src/Descriptor.php
+++ src/Descriptor.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ntentan\atiaa;
 
 /**
@@ -7,28 +8,26 @@ namespace ntentan\atiaa;
  * classes which provide the details of the actual database items. The main work
  * of this class is to format the description into a common format.
  */
-abstract class Descriptor
-{   
+abstract class Descriptor {
+
     /**
      * An instance of the database driver used for accessing the database
      * system.
      * @var \ntentan\atiaa\Driver;
      */
     protected $driver;
-    
     private $cleanDefaults = false;
-    
-    public function __construct($driver)
-    {
+
+    public function __construct($driver) {
         $this->driver = $driver;
     }
-    
+
     /**
      * Returns a list of schemata available on the database.
      * @return array
      */
     abstract protected function getSchemata();
-    
+
     /**
      * Retrieve the names of all the tables in a given schema. 
      * The array returned must be a list of structured arrays which have `name`
@@ -43,7 +42,7 @@ abstract class Descriptor
      * @return array<array>  
      */
     abstract protected function getTables($schema, $requestedTables, $includeViews);
-    
+
     /**
      * Retrieve descriptions of all the columns available in a given table as an array.
      * The array returned must contain structured arrays with the following keys.
@@ -70,7 +69,7 @@ abstract class Descriptor
      * @return array<array<string>>
      */
     abstract protected function getColumns(&$table);
-    
+
     /**
      * Retrieve the descriptions of all the views of a given schema in a array.
      * The array returned must contain structured arrays with the following keys.
@@ -88,7 +87,7 @@ abstract class Descriptor
      * @return array<array<string>>
      */
     abstract protected function getViews(&$schema);
-    
+
     /**
      * Retrieve the description of a primary key on a given table.
      * The description returned must be an array which contains structured
@@ -109,7 +108,7 @@ abstract class Descriptor
      * @return array<array<string>>
      */
     abstract protected function getPrimaryKey(&$table);
-    
+
     /**
      * Retrieve the description of unique keys on a given table.
      * The description returned must be an array which contains structured
@@ -129,7 +128,7 @@ abstract class Descriptor
      * @return array<array<string>>
      */
     abstract protected function getUniqueKeys(&$table);
-    
+
     /**
      * Retrieve the description of foreign keys on a given table.
      * The description returned must be an array which contains structured
@@ -163,9 +162,9 @@ abstract class Descriptor
      * @param array $table An array which contains the name of the table as it's
      *     `name` key and the schema of the table as it's `schema` key.
      * @return array<array<string>>
-     */    
+     */
     abstract protected function getForeignKeys(&$table);
-    
+
     /**
      * Retrieve the description of indices on a given table.
      * The description returned must be an array which contains structured
@@ -183,9 +182,9 @@ abstract class Descriptor
      * @param array $table An array which contains the name of the table as it's
      *     `name` key and the schema of the table as it's `schema` key.
      * @return array<array<string>>
-     */    
+     */
     abstract protected function getIndices(&$table);
-    
+
     /**
      * Returns a boolean value which tells whether a table has an auto incrementing
      * key or not.
@@ -193,44 +192,38 @@ abstract class Descriptor
      * @return boolean
      */
     abstract protected function hasAutoIncrementingKey(&$table);
-    
+
     /**
      * Returns the description of the database as an array.
      * 
      * @return array
      */
-    public function describe()
-    {
+    public function describe() {
         $defaultSchema = $this->driver->getDefaultSchema();
         $description = array(
             'schemata' => array(),
         );
-        
+
         $schemata = $this->getSchemata();
-        
-        foreach($schemata as $schema)
-        {
-            if($schema['name'] == $defaultSchema)
-            {
+
+        foreach ($schemata as $schema) {
+            if ($schema['name'] == $defaultSchema) {
                 $description['tables'] = $this->describeTables($defaultSchema);
                 $description['views'] = $this->describeViews($defaultSchema);
-            }
-            else
-            {
+            } else {
                 $description['schemata'][$schema['name']]['name'] = $schema['name'];
-                $description['schemata'][$schema['name']]['tables'] = $this->describeTables($schema['name']);                
-                $description['schemata'][$schema['name']]['views'] = $this->describeViews($schema['name']);                
+                $description['schemata'][$schema['name']]['tables'] = $this->describeTables($schema['name']);
+                $description['schemata'][$schema['name']]['views'] = $this->describeViews($schema['name']);
             }
         }
-        
-        return $description;       
+
+        return $description;
     }
-    
-    public function setCleanDefaults($cleanDefaults)
-    {
+
+    public function setCleanDefaults($cleanDefaults) {
         $this->cleanDefaults = $cleanDefaults;
     }
-    
+
     /**
      * Throws exceptions for which are found in the list of requested tables
      * but not found in the list of found tables.
@@ -239,35 +232,28 @@ abstract class Descriptor
      * @param array $requestedTables
      * @throws exceptions\TableNotFoundException
      */
-    private function throwTableExceptions($tables, $requestedTables)
-    {
+    private function throwTableExceptions($tables, $requestedTables) {
         $foundTables = array();
-        foreach($tables as $table)
-        {
+        foreach ($tables as $table) {
             $foundTables[] = $table['name'];
         }
-        
-        foreach($requestedTables as $requestedTable)
-        {
-            if(array_search($requestedTable, $foundTables) === false)
-            {
+
+        foreach ($requestedTables as $requestedTable) {
+            if (array_search($requestedTable, $foundTables) === false) {
                 throw new exceptions\TableNotFoundException("$requestedTable not found on target database.");
             }
         }
     }
-    
-    public function describeTables($schema, $requestedTables = array(), $includeViews = false)
-    {
+
+    public function describeTables($schema, $requestedTables = array(), $includeViews = false) {
         $description = array();
         $tables = $this->getTables($schema, $requestedTables, $includeViews);
-        
-        if(count($requestedTables) > 0 && count($tables) < count($requestedTables))
-        {
+
+        if (count($requestedTables) > 0 && count($tables) < count($requestedTables)) {
             $this->throwTableExceptions($tables, $requestedTables);
         }
-        
-        foreach($tables as $table)
-        {
+
+        foreach ($tables as $table) {
             $table['columns'] = $this->describeColumns($table);
             $table['primary_key'] = $this->describePrimaryKey($table);
             $table['unique_keys'] = $this->describeUniqueKeys($table);
@@ -275,105 +261,86 @@ abstract class Descriptor
             $table['indices'] = $this->describeIndices($table);
             $table['auto_increment'] = $this->hasAutoIncrementingKey($table);
             $table['schema'] = $this->fixSchema($table['schema']);
-            
+
             $description[$table['name']] = $table;
         }
-        return $description;        
+        return $description;
     }
-    
-    private function describeColumns($table)
-    {
+
+    private function describeColumns($table) {
         $columns = array();
         $columnDetails = $this->getColumns($table);
-        foreach($columnDetails as $column)
-        {
+        foreach ($columnDetails as $column) {
             $columns[$column['name']] = $column;
             $columns[$column['name']]['nulls'] = $columns[$column['name']]['nulls'] == 'YES' ? true : false;
-            
-            if($this->cleanDefaults) {
+
+            if ($this->cleanDefaults) {
                 $columns[$column['name']]['default'] = $this->cleanDefaultValue($column['default']);
             }
         }
-        
-        return $columns;        
+
+        return $columns;
     }
-    
-    private function describeViews($schema)
-    {
+
+    private function describeViews($schema) {
         $description = array();
         $views = $this->getViews($schema);
-        foreach($views as $view)
-        {
+        foreach ($views as $view) {
             $description[$view['name']] = array(
                 'name' => $view['name'],
                 'schema' => $view['schema'],
                 'definition' => $view['definition']
             );
         }
-        return $description;        
+        return $description;
     }
-    
-    private function describePrimaryKey($table)
-    {
+
+    private function describePrimaryKey($table) {
         return $this->describeKey($this->getPrimaryKey($table));
     }
 
-    private function describeUniqueKeys($table)
-    {
+    private function describeUniqueKeys($table) {
         return $this->describeKey($this->getUniqueKeys($table));
     }
-    
-    private function describeForeignKeys($table)
-    {
+
+    private function describeForeignKeys($table) {
         return $this->describeKey($this->getForeignKeys($table));
     }
-    
-    private function describeIndices($table)
-    {
+
+    private function describeIndices($table) {
         return $this->describeKey($this->getIndices($table));
     }
-    
-    private function describeKey($constraintColumns)
-    {
+
+    private function describeKey($constraintColumns) {
         $constraints = array();
-        foreach($constraintColumns as $column)
-        {
+        foreach ($constraintColumns as $column) {
             $name = $column['name'];
             unset($column['name']);
-            foreach($column as $key => $value)
-            {
-                if($key == 'column' || $key == 'foreign_column')
-                {
+            foreach ($column as $key => $value) {
+                if ($key == 'column' || $key == 'foreign_column') {
                     $constraints[$name]["{$key}s"][] = $value;
-                }
-                else
-                {
-                    if($key === 'schema' || $key === 'foreign_schema') 
-                    {
+                } else {
+                    if ($key === 'schema' || $key === 'foreign_schema') {
                         $value = $this->fixSchema($value);
                     }
                     $constraints[$name][$key] = $value;
                 }
             }
         }
-        return $constraints;    
+        return $constraints;
     }
-    
-    private function fixSchema($schema)
-    {
-        $defaultSchema = $this->driver->getDefaultSchema();        
-        if($schema == false || $schema == $defaultSchema)
-        {
+
+    private function fixSchema($schema) {
+        $defaultSchema = $this->driver->getDefaultSchema();
+        if ($schema == false || $schema == $defaultSchema) {
             return '';
-        }
-        else
-        {
+        } else {
             return $schema;
         }
-    }   
-    
-    protected function cleanDefaultValue($defaultValue)
-    {
+    }
+
+    protected function cleanDefaultValue($defaultValue) {
         return $defaultValue;
     }
+
 }
