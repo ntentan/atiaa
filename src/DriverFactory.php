fcb46bebb88fa5cee4fd3940f3f2b8d65467541d
James Ainooson <jainooson@gmail.com>
2017-09-14 07:14:46 -0500

    Minor documentation

--- src/DriverFactory.php
+++ src/DriverFactory.php
@@ -1,26 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ekow
- * Date: 9/4/17
- * Time: 10:50 AM
- */
 
 namespace ntentan\atiaa;
 
 use ntentan\utils\Text;
 
-
 class DriverFactory
 {
     private $config;
 
+    /**
+     * Create an instance of the driver factory with the connection configurations.
+     * 
+     * @param array $config
+     */
     public function __construct(array $config)
     {
         $this->config = $config;
     }
 
-    public function createDriver()
+    /**
+     * Create a new driver based on the configuration in the factory.
+     * 
+     * @return \ntentan\atiaa\Driver
+     */
+    public function createDriver() : Driver
     {
         $classname = '\ntentan\atiaa\drivers\\' . Text::ucamelize($this->config['driver']) . "Driver";
         return new $classname($this->config);
